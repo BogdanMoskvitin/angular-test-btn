fdf0df471474ff0633e09f30747f74732acb5609
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2021-08-23 11:25:26 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test-btn';
+
+  isActive1: boolean = false;
+  isActive2: boolean = false;
+  isActive3: boolean = false;
+  value: string = "1";
+
+  clickBtn(){
+    this.isActive1 = true;
+    this.isActive2 = true;
+    this.isActive3 = true;
+  }
+  clickBtn1(){
+    this.value = "1";
+    this.isActive1 = false;
+  }
+  clickBtn2(){
+    this.value = "2";
+    this.isActive2 = false;
+  }
+  clickBtn3(){
+    this.value = "3";
+    this.isActive3 = false;
+  }
 }
